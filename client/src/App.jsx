6282a3f50c66b0ee0bb2a983e26496e80658347b
Avinash Kumar <avinashkumartirakachuri9@gmail.com>
2026-05-31 15:38:13 +0530

    Update API URLs for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -121,7 +121,7 @@ useEffect(() => {
     setLoading(true);
 
     const res = await axios.post(
-      "http://127.0.0.1:5000/predict",
+      "http://placetector.onrender.com/predict",
       form
     );
 
